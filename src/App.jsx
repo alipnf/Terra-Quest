e7c1f6fc2aa4056d53572403fe7d423a86f73e00
alipnf <alipnf@proton.me>
2024-11-10 00:36:10 +0700

    feat(pages): add achievements

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import NpcDetail from "./pages/NpcDetail";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Quest from "./pages/Quest";
+import Achievements from "./pages/Achievements";
 
 export default function App() {
   return (
@@ -13,6 +14,7 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/npc" element={<NpcDetail />} />
         <Route path="/quest" element={<Quest />} />
+        <Route path="/achievements" element={<Achievements />} />
       </Routes>
       <Footer />
     </Router>
---
+++ src/pages/Achievements.jsx
@@ -0,0 +1,9 @@
+import AchievementsContent from "../components/achievements/AchievementsContent";
+
+export default function Achievements() {
+  return (
+    <div className="min-h-screen bg-base-100 p-4">
+      <AchievementsContent />
+    </div>
+  );
+}
