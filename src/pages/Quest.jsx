076ee70fd8a68e3d82e84838480060bf5a8b2348
alipnf <alipnf@proton.me>
2024-11-09 22:30:39 +0700

    feat(quest): add quest feature
    Add quest feature to the application.
    This feature allows users to generate, take, and complete quests from NPCs.
    It also includes a quest list that displays all available quests.

---
+++ src/pages/Quest.jsx
@@ -0,0 +1,9 @@
+import QuestContent from "../components/quest/QuestContent";
+
+export default function Quest() {
+  return (
+    <div className="container mx-auto min-h-screen p-4">
+      <QuestContent />
+    </div>
+  );
+}
