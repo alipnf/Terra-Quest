254c386aa02998b4d8b39c6195d7ff3ec708a318
alipnf <alipnf@proton.me>
2024-11-12 00:17:45 +0700

    feat(quest): implemented quest detail page with chat
    Refactored the quest component to include a detail page with a chat window.
    Added state management and event handling for sending and receiving messages.
    Integrated the Gemini AI service for chat functionality.

---
+++ src/components/quest/InputChat.jsx
@@ -0,0 +1,30 @@
+import { useRef, useEffect } from "react";
+
+export default function InputChat({ handleSendMessage, setInput, input }) {
+  const inputChat = useRef();
+  useEffect(() => {
+    inputChat.current.focus();
+  }, []);
+
+  return (
+    <div className="sticky left-0 right-0 mt-4 flex w-full px-5 bottom-10 mb-10">
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        placeholder="Type here"
+        className="input input-bordered mr-2 w-full"
+        ref={inputChat}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            handleSendMessage();
+            setInput("");
+          }
+        }}
+      />
+      <button onClick={handleSendMessage} className="btn btn-primary">
+        Send
+      </button>
+    </div>
+  );
+}
