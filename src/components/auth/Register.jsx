69e3a287de44d793aa5fa8773199b9fa9b74ecf2
alipnf <alipnf@proton.me>
2024-11-13 16:23:55 +0700

    feat(auth): implement custom register hook
    Uses a custom hook for registration, moved the registration related code from Register.jsx to useRegister.js for better organization and maintainability.

--- src/components/auth/Register.jsx
+++ src/components/auth/Register.jsx
@@ -1,56 +1,28 @@
-import { useState, useEffect } from "react";
+import { useRegister } from "../../hooks/useRegister";
 import { useNavigate } from "react-router-dom";
-import { useUserStore } from "../../stores/useUserstore";
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-import { auth } from "../../services/firebase/firebaseConfig";
 
 export default function Register() {
-  const { register, error, setUser, setError } = useUserStore();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [name, setName] = useState("");
-  const [showSuccessModal, setShowSuccessModal] = useState(false);
+  const {
+    email,
+    setEmail,
+    password,
+    setPassword,
+    name,
+    setName,
+    handleSubmit,
+    handleGoogleRegister,
+    showSuccessModal,
+    closeSuccessModal,
+    error,
+  } = useRegister();
+
   const navigate = useNavigate();
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    register(email, password, name, () => {
-      if (!error) {
-        setShowSuccessModal(true);
-      }
-    });
-  };
-
-  const handleGoogleRegister = async () => {
-    const provider = new GoogleAuthProvider();
-    try {
-      const result = await signInWithPopup(auth, provider);
-      const user = result.user;
-      setUser(user);
-    } catch (error) {
-      console.error("Error registering with Google:", error);
-    }
-  };
-
-  const closeSuccessModal = () => {
-    setShowSuccessModal(false);
-    navigate("/login"); // Arahkan ke halaman login setelah menutup modal
-  };
-
-  useEffect(() => {
-    if (error) {
-      const timer = setTimeout(() => {
-        setError(null);
-      }, 3000); // Error hilang setelah 5 detik
-      return () => clearTimeout(timer);
-    }
-  }, [error, setError]);
   return (
-    <div className="min-h-screen flex items-center justify-center">
-      <div className="card xl:w-5/12  bg-base-100 shadow-xl">
+    <div className="md:min-h-screen p-5 flex items-center justify-center">
+      <div className="card xl:w-5/12 bg-base-100 shadow-2xl">
         <div className="card-body">
           <p className="card-title">Daftar ke TerraQuest</p>
-          <p> Buat akun untuk memulai petualanganmu </p>
+          <p>Buat akun untuk memulai petualanganmu</p>
           <form onSubmit={handleSubmit}>
             <div className="grid w-full gap-4">
               <div className="form-control">
@@ -124,7 +96,7 @@ export default function Register() {
       </div>
 
       {showSuccessModal && (
-        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 p-10">
           <div className="bg-white p-6 rounded-lg shadow-lg text-center">
             <h2 className="text-2xl font-semibold mb-4">
               Registrasi Berhasil!
