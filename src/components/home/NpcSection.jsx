3753a8c25e5528fdad48c6daa7f4b5a531e99126
alipnf <alipnf@proton.me>
2024-11-16 14:05:15 +0700

    fix(home): Implement responsive image

--- src/components/home/NpcSection.jsx
+++ src/components/home/NpcSection.jsx
@@ -6,16 +6,24 @@ const npcs = [
     name: "Greenia",
     title: "Penjaga alam dari masa lalu",
     description: "Pelajari kebijaksanaan alam untuk melestarikan lingkungan.",
-    image:
-      "https://res.cloudinary.com/dpye48lbg/image/upload/v1731735978/greenia_unkuiw.webp",
+    image: {
+      mobile:
+        "https://res.cloudinary.com/dpye48lbg/image/upload/w_600,h_400,c_fill/v1731735978/greenia_unkuiw.webp", // smaller image for mobile
+      desktop:
+        "https://res.cloudinary.com/dpye48lbg/image/upload/v1731735978/greenia_unkuiw.webp", // original image for desktop
+    },
   },
   {
     icon: Zap,
     name: "Okta",
     title: "Utusan dari masa depan",
     description: "Jelajahi inovasi hijau untuk masa kini.",
-    image:
-      "https://res.cloudinary.com/dpye48lbg/image/upload/v1731660735/okta_qmwzbu.webp",
+    image: {
+      mobile:
+        "https://res.cloudinary.com/dpye48lbg/image/upload/w_600,h_400,c_fill/v1731660735/okta_qmwzbu.webp", // smaller image for mobile
+      desktop:
+        "https://res.cloudinary.com/dpye48lbg/image/upload/v1731660735/okta_qmwzbu.webp", // original image for desktop
+    },
   },
 ];
 
@@ -26,9 +34,12 @@ export default function NPCSection() {
         <div key={index} className="card bg-neutral shadow-lg">
           <div className="card-body">
             <img
-              src={npc.image}
+              src={npc.image.mobile} // Default image for mobile
               alt={npc.name}
               className="object-cover w-full h-48 md:h-80"
+              srcSet={`${npc.image.mobile} 600w, ${npc.image.desktop} 1200w`} // responsive images
+              sizes="(max-width: 767px) 100vw, (min-width: 768px) 50vw"
+              loading="lazy"
             />
             <h2 className="card-title flex items-center text-neutral-content">
               <npc.icon className="mr-2 h-6 w-6" /> {npc.name}
