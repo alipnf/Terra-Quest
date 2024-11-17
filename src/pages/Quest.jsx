831f6e93a5be71009596d6241cdb563f1062f603
alipnf <alipnf@proton.me>
2024-11-17 15:40:42 +0700

    refactor(quest): add theme to store

--- src/pages/Quest.jsx
+++ src/pages/Quest.jsx
@@ -1,9 +1,9 @@
 import QuestContent from "../components/quest/QuestContent";
 
-export default function Quest({ setTheme }) {
+export default function Quest() {
   return (
     <div className="container mx-auto min-h-screen p-4">
-      <QuestContent setTheme={setTheme} />
+      <QuestContent />
     </div>
   );
 }
